169681ac18ee51a6aecab726080ca3ad4a3561b0
Diyar68 <229304603+Diyar68@users.noreply.github.com>
2026-06-19 23:21:16 +0200

    worked on TaskTracker Methods

--- src/task_tracker_cli/TaskTracker.java
+++ src/task_tracker_cli/TaskTracker.java
@@ -1,5 +1,73 @@
 package task_tracker_cli;
 
-public class TaskTracker {
+import java.util.ArrayList;
 
+public class TaskTracker {
+	private ArrayList<Task> tasks = new ArrayList<>();
+
+	public TaskTracker() {
+
+	}
+
+	public void addTask(String description) {
+		int id = generateNextId();
+
+		Task task = new Task(id, description);
+
+		tasks.add(task);
+	}
+
+	public void deleteTask(int id) {
+		Task task = findById(id);
+		
+		if(task == null) {
+			System.out.print("Task not found");
+			return;
+		}
+		
+		tasks.remove(task);
+	}
+
+	public void updateTask(int id, String description) {
+
+	}
+
+	public void markInProgress(int id) {
+
+	}
+
+	public void markDone(int id) {
+
+	}
+
+	public ArrayList<Task> getAllTasks() {
+		return tasks;
+	}
+
+	public void listByStatus(TaskStatus status) {
+
+	}
+
+	private Task findById(int id) {
+
+		for (Task task : tasks) {
+			if (task.getId() == id) {
+				return task;
+			}
+		}
+		
+		return null;
+	}
+
+	private int generateNextId() {
+		int maxId = 0;
+
+		for (Task task : tasks) {
+			if (task.getId() > maxId) {
+				maxId = task.getId();
+			}
+		}
+
+		return maxId + 1;
+	}
 }
